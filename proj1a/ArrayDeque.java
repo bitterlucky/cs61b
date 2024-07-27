002dbe20427bdf26c36c3755395f3b3faa6984cd
bitterlucky <925182053@qq.com>
2024-07-27 16:04:26 +0800

    proj0

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -1,2 +1,122 @@
-public class ArrayDeque {
+public class ArrayDeque<T> {
+    private int size;
+    private int maxSize;
+    private int nextFirst;
+    private int nextLast;
+    private T[] arrays;
+    private static final double parseRate = 0.25;
+    public ArrayDeque() {
+        size = 0;
+        maxSize = 8;
+        nextFirst = 0;
+        nextLast = 1;
+        arrays = (T[]) new Object[maxSize];
+    }
+    private void shrink() {
+        T[] tmpArrays = (T[]) new Object[maxSize / 2];
+        int first = parse(nextFirst + 1);
+        for (int i = 0; i < size; i++) {
+            tmpArrays[i] = arrays[first];
+            first = parse(first + 1);
+        }
+        nextLast = size;
+        arrays = (T[]) new Object[maxSize / 2];
+        for (int i = 0; i < size; i++) {
+            arrays[i] = tmpArrays[i];
+        }
+        maxSize = maxSize / 2;
+        nextFirst = maxSize - 1;
+
+    }
+    private void grow() {
+
+        T[] tmpArrays = (T[]) new Object[maxSize * 2];
+        int first = parse(nextFirst + 1);
+        for (int i = 0; i < size; i++) {
+            tmpArrays[i] = arrays[first];
+            first = parse(first + 1);
+        }
+
+        nextLast = size;
+        arrays = (T[]) new Object[maxSize * 2];
+        for (int i = 0; i < size; i++) {
+            arrays[i] = tmpArrays[i];
+        }
+        maxSize = maxSize * 2;
+        nextFirst = maxSize - 1;
+    }
+    private int parse(int n) {
+        if (n == maxSize) {
+            return 0;
+        } else if (n == -1) {
+            return maxSize - 1;
+        } else {
+            return n;
+        }
+    }
+    public void addFirst(T item) {
+        if (size == maxSize) {
+            grow();
+        }
+        arrays[nextFirst] = item;
+        nextFirst--;
+        nextFirst = parse(nextFirst);
+        size++;
+
+    }
+    public void addLast(T item) {
+        if (size == maxSize) {
+            grow();
+        }
+        arrays[nextLast] = item;
+        nextLast++;
+        nextLast = parse(nextLast);
+        size++;
+
+    }
+    public boolean isEmpty() {
+        return size == 0;
+    }
+    public int size() {
+        return size;
+    }
+    public void printDeque() {
+        int first = parse(nextFirst + 1);
+        for (int i = 0; i < size; i++) {
+            System.out.print(arrays[first]);
+            System.out.print(" ");
+            first = parse(first + 1);
+        }
+    }
+    public T removeFirst() {
+        if (maxSize >= 16 && size < parseRate * maxSize) {
+            shrink();
+        }
+        nextFirst = parse(nextFirst + 1);
+        T item = arrays[nextFirst];
+        size--;
+        return item;
+
+    }
+    public T removeLast() {
+        if (maxSize >= 16 && size < parseRate * maxSize) {
+            shrink();
+        }
+        nextLast = parse(nextLast - 1);
+        T item = arrays[nextLast];
+        size--;
+        return item;
+
+
+    }
+    public T get(int index) {
+        if (index >= size || index < 0) {
+            return null;
+        }
+        int first = parse(nextFirst + 1);
+        for (int i = 0; i < index; i++) {
+            first = parse(first + 1);
+        }
+        return arrays[first];
+    }
 }
--- proj1a/ArrayDequeTest.java
+++ proj1a/ArrayDequeTest.java
@@ -1,3 +1,80 @@
+import org.junit.Test;
+
 public class ArrayDequeTest {
-    
+    @Test
+    public void testAddFirst() {
+        ArrayDeque<Integer> integerArrayDeque = new ArrayDeque<>();
+        for (int i = 0; i < 15; i++) {
+            integerArrayDeque.addFirst(i);
+        }
+        integerArrayDeque.printDeque();
+    }
+    @Test
+    public void testAddLastAndPrintDeque() {
+        ArrayDeque<Integer> integerArrayDeque = new ArrayDeque<>();
+        for (int i = 0; i < 15; i++) {
+            integerArrayDeque.addLast(i);
+        }
+        integerArrayDeque.printDeque();
+
+    }
+    @Test
+    public void testGetIllegal() {
+        ArrayDeque<Integer> integerArrayDeque = new ArrayDeque<>();
+        for (int i = 0; i < 15; i++) {
+            integerArrayDeque.addLast(i);
+        }
+        System.out.println(integerArrayDeque.get(16));
+    }
+
+    @Test
+    public void testGetLegal() {
+        ArrayDeque<Integer> integerArrayDeque = new ArrayDeque<>();
+        for (int i = 0; i < 15; i++) {
+            integerArrayDeque.addLast(i);
+        }
+        System.out.println(integerArrayDeque.get(14));
+    }
+    @Test
+    public void testRemoveFirstExceedBound() {
+        ArrayDeque<Integer> integerArrayDeque = new ArrayDeque<>();
+        for (int i = 0; i < 30; i++) {
+            integerArrayDeque.addLast(i);
+        }
+        for (int i = 0; i < 18; i++) {
+            integerArrayDeque.removeFirst();
+        }
+        integerArrayDeque.printDeque();
+    }
+    @Test
+    public void testRemoveFirstWithoutExceedBound() {
+        ArrayDeque<Integer> integerArrayDeque = new ArrayDeque<>();
+        for (int i = 0; i < 15; i++) {
+            integerArrayDeque.addLast(i);
+        }
+        integerArrayDeque.removeFirst();
+        integerArrayDeque.removeFirst();
+        integerArrayDeque.printDeque();
+    }
+    @Test
+    public void testRemoveLastExceedBound() {
+        ArrayDeque<Integer> integerArrayDeque = new ArrayDeque<>();
+        for (int i = 0; i < 30; i++) {
+            integerArrayDeque.addLast(i);
+        }
+        for (int i = 0; i < 18; i++) {
+            integerArrayDeque.removeLast();
+        }
+        integerArrayDeque.printDeque();
+    }
+    @Test
+    public void testRemoveLastWithoutExceedBound() {
+        ArrayDeque<Integer> integerArrayDeque = new ArrayDeque<>();
+        for (int i = 0; i < 15; i++) {
+            integerArrayDeque.addLast(i);
+        }
+        integerArrayDeque.removeLast();
+        integerArrayDeque.removeLast();
+        integerArrayDeque.printDeque();
+    }
 }

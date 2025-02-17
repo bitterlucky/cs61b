91ebbc9e20ff165c28f99e8266e4f39022ba8ede
bitterlucky <925182053@qq.com>
2025-02-17 17:22:32 +0800

    proj1

--- proj1/deque/MaxArrayDeque.java
+++ proj1/deque/MaxArrayDeque.java
@@ -4,8 +4,8 @@ import java.util.Comparator;
 
 public class MaxArrayDeque<T> extends ArrayDeque<T> {
     private Comparator<T> c;
-    public MaxArrayDeque(Comparator<T> c) {
-        this.c = c;
+    public MaxArrayDeque(Comparator<T> comparator) {
+        this.c = comparator;
     }
     public T max() {
         if (size() == 0) {
@@ -20,14 +20,14 @@ public class MaxArrayDeque<T> extends ArrayDeque<T> {
         }
         return maxElement;
     }
-    public T max(Comparator<T> c) {
+    public T max(Comparator<T> comparator) {
         if (size() == 0) {
             return null;
         }
         T maxElement = get(0);
         for (int i = 0; i < size(); i++) {
             T thisElement = get(i);
-            if (c.compare(thisElement, maxElement) > 0) {
+            if (comparator.compare(thisElement, maxElement) > 0) {
                 maxElement = thisElement;
             }
         }
